2a5b3d48be1d39e84c28209ee84bf8c6a58bb5e1
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2021-08-16 00:04:58 +0530

    Problem 8

---
+++ Euler/Problems/Problem8.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Euler.Problems
+{
+    [Description("Largest product in a series")]
+    public class Problem8 : AbstractProblem
+    {
+        public override void Execute()
+        {
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            string input = @"7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+
+            List<int> numbers = new List<int>();
+
+            foreach (char a in input)
+            {
+                numbers.Add(Convert.ToInt32(a.ToString()));
+            }
+
+            List<double> sums = new List<double>();
+
+            int noOfAdjusentDigits = 13;
+            int resetIndex = 0;
+            for (int index = 0; index <= numbers.Count - 13; index++)
+            {
+                int sum = 1;
+                string multipleNumber = "";
+                for (int subIndex = resetIndex; subIndex < noOfAdjusentDigits; subIndex++)
+                {
+                    if (numbers[subIndex] == 0)
+                    {
+                        break;
+                    }
+
+                    sum = sum * numbers[subIndex];
+                    multipleNumber = multipleNumber + numbers[subIndex].ToString();
+                    if (subIndex == noOfAdjusentDigits - 1)
+                    {
+                        keyValuePairs.Add(multipleNumber, sum);
+                        sums.Add(sum);
+                    }
+                }
+                resetIndex++;
+                noOfAdjusentDigits++;
+            }
+
+            Dictionary<string, int> sortedList = new Dictionary<string, int>();
+
+            int largest = 0;
+            string largestnums = String.Empty;
+
+            int sortidx = 0;
+
+
+            foreach (var a in keyValuePairs)
+            {
+                Console.WriteLine(a.Key);
+
+                if (a.Value > largest)
+                {
+                    largest = a.Value;
+                    largestnums = a.Key;
+                }
+            }
+            Console.WriteLine("laregest : " + largestnums);
+
+
+        }
+    }
+}
